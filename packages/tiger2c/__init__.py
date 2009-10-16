095de5dcb58ea40e0bfe256e3d271640c53e30ad
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2009-10-15 23:21:42 -0400

    Importando esqueleto del proyecto.
    Importando la estructura del paquete Python principal del proyecto y un script
    para ejecutar Tiger2C desde la linea de comandos.

---
+++ packages/tiger2c/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Paquete principal de Tiger2C. 
+"""
+
+__version__ = '0.1'
+
+__authors__ = (
+    'Yasser González Fernández <yglez@uh.cu>',
+    'Ariel Hernández Amador <gnuaha7@uh.cu>',
+)
+
+
+def translate(tiger_filename, c_filename):
+    """Traduce un programa Tiger a un programa C equivalente.
+    """
---
+++ packages/tiger2c/errors.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Contiene la jerarquía de las excepciones lanzadas en el paquete.
+"""
+
+
+class Tiger2CError(Exception):
+    """Clase base de todas las excepciones lanzadas en el paquete.
+    """
