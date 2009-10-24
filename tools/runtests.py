7049d66b73abe8ff2f1e1a91d2c2485bcc67d7a4
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2009-10-24 03:02:01 -0400

    Modificado framework de pruebas para comprobar errores de compilación.

--- tools/runtests.py
+++ tools/runtests.py
@@ -15,31 +15,36 @@ TIGER2C_SCRIPT = os.path.abspath(os.path.join(SRC_DIR, 'scripts', 'tiger2c.py'))
 
 TIGER2C_CMD = ['python', TIGER2C_SCRIPT, '-c']
 
-TEST_DIR = os.path.abspath(os.path.join(SRC_DIR, 'tests'))
+TESTS_DIR = os.path.abspath(os.path.join(SRC_DIR, 'tests'))
+
+SUCCCESS_DIR = os.path.abspath(os.path.join(TESTS_DIR, 'success'))
+
+FAIL_DIR = os.path.abspath(os.path.join(TESTS_DIR, 'fail'))
 
 
 class TigerTestCase(unittest.TestCase):
     """
-    Representa la ejecución de un programa Tiger de prueba.
+    Clase base para ambos tipos de pruebas.
     """
     
-    def __init__(self, tiger_file):
+    def __init__(self, parent_dir, tiger_file):
         """
         Inicializa el la prueba.
         """
         super(TigerTestCase, self).__init__()
-        self._tiger_file = os.path.join(TEST_DIR, tiger_file)
-        self._exec_file = os.path.join(TEST_DIR, tiger_file[:-4])
-        self._tiger2c_cmd = TIGER2C_CMD
-        self._tiger2c_cmd.append(self._tiger_file)
-        self._tiger2c_cmd.append(self._exec_file)
-        self._in_file = os.path.join(TEST_DIR, tiger_file[:-4] + '.in')
+        self._tiger_file = os.path.join(parent_dir, tiger_file)
+        self._exec_file = os.path.join(parent_dir, tiger_file[:-4])
+        self._tiger2c_cmd = TIGER2C_CMD + [self._tiger_file, self._exec_file]
+        self._in_file = os.path.join(parent_dir, tiger_file[:-4] + '.in')
         if not os.path.isfile(self._in_file):
             self._in_file = None
-        self._out_file = os.path.join(TEST_DIR, tiger_file[:-4] + '.out')
+        self._out_file = os.path.join(parent_dir, tiger_file[:-4] + '.out')
         if not os.path.isfile(self._out_file):
             self._out_file = None
-        self._tmp_file = os.path.join(TEST_DIR, tiger_file[:-4] + '.tmp')
+        self._err_file = os.path.join(parent_dir, tiger_file[:-4] + '.err')
+        if not os.path.isfile(self._err_file):
+            self._err_file = None
+        self._tmp_file = os.path.join(parent_dir, tiger_file[:-4] + '.tmp')
         
     def shortDescription(self):
         """
@@ -47,22 +52,11 @@ class TigerTestCase(unittest.TestCase):
         """
         return os.path.basename(self._tiger_file)
    
-    def runTest(self):
+    def failIfDifferent(self, first_file, second_file):
         """
-        Ejecuta la prueba.
+        Falla si los archivos son diferentes.
         """
-        # Compile the program.
-        if subprocess.call(self._tiger2c_cmd) != 0 or not os.path.isfile(self._exec_file):
-            self.fail('Compilation failed!')
-        # Execute the program.
-        exec_stdin = open(self._in_file) if self._in_file is not None else self._in_file
-        exec_stdout = open(self._tmp_file, 'w')
-        subprocess.call([self._exec_file], stdin=exec_stdin, stdout=exec_stdout)
-        if exec_stdin is not None:
-            exec_stdin.close()
-        exec_stdout.close()
-        # Compare the output of the programs.
-        diff_cmd = ['diff', self._tmp_file, self._out_file]
+        diff_cmd = ['diff', first_file, second_file]
         with  open('/dev/null', 'w') as devnull:
             if subprocess.call(diff_cmd, stdout=devnull, stderr=devnull) != 0:
                 self.fail('Output does not match!')
@@ -77,15 +71,65 @@ class TigerTestCase(unittest.TestCase):
             os.remove(self._tmp_file)
 
 
+class SuccessTigerTestCase(TigerTestCase):
+    """
+    Representa una prueba de éxito.
+    
+    El programa Tiger utilizado en esta prueba deberá compilarse sin errores
+    y al ejecutarse, recibiendo el archivo .in como entrada standard, su 
+    salida debe coincidir con el contenido del archivo .out. 
+    """
+    
+    def runTest(self):
+        """
+        Ejecuta la prueba.
+        """
+        # Compile the program.
+        if subprocess.call(self._tiger2c_cmd) != 0 or not os.path.isfile(self._exec_file):
+            self.fail('Compilation failed!')
+        # Execute the program.
+        exec_stdin = open(self._in_file) if self._in_file is not None else self._in_file
+        with open(self._tmp_file, 'w') as exec_stdout:
+            subprocess.call([self._exec_file], stdin=exec_stdin, stdout=exec_stdout)
+            if exec_stdin is not None:
+                exec_stdin.close()
+        # Compare the output of the programs.
+        self.failIfDifferent(self._tmp_file, self._out_file)                    
+        
+    
+class FailTigerTestCase(TigerTestCase):
+    """
+    Representa una prueba de fallo.
+    
+    Tiger2C deberá fallar al intentar compilar el programa Tiger utilizado en
+    esta prueba y el mensaje de error que imprima en la salida estándard de
+    errores deberá coincidir con el contenido del archivo .err.
+    """
+    
+    def  runTest(self):
+        """
+        Ejecuta la prueba.
+        """
+        # Try to compile the program.
+        with open(self._err_file, 'w') as tiger2c_stderr: 
+            if subprocess.call(self._tiger2c_cmd, stderr=tiger2c_stderr) != 1:
+                self.fail('Compilation succeded and it should fail!')
+        # Compare the error output.
+        self.failIfDifferent(self._tmp_file, self._err_file)        
+
+
 def main():
     """
     Función principal del script.
     """
     suite = unittest.TestSuite()
     runner = unittest.TextTestRunner(verbosity=2);
-    for tiger_file in [f for f in os.listdir(TEST_DIR) if f.endswith('.tig')]:
-        test_case = TigerTestCase(tiger_file)
+    for tiger_file in [f for f in os.listdir(SUCCCESS_DIR) if f.endswith('.tig')]:
+        test_case = SuccessTigerTestCase(SUCCCESS_DIR, tiger_file)
         suite.addTest(test_case)
+    for tiger_file in [f for f in os.listdir(FAIL_DIR) if f.endswith('.tig')]:
+        test_case = FailTigerTestCase(FAIL_DIR, tiger_file)
+        suite.addTest(test_case)        
     runner.run(suite)
 
 
