a88beff78f9fb1dbb6b5fbdf5f43a1806ac6b4a0
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-01-30 15:45:30 -0500

    Añadido cuerpo de la función translate().
    Implementada translate() en función de funciones encargadas de realizar el análisis léxico-gráfico y sintáctico, checkeo semántico y generación de código. Cree cada una de estas funciones con cuerpo vacío.
    
    Creadas excepciones que se lanzarán cuando ocurra un error sintáctico, semántico o durante la generación de código. Estas excepciones heredan de PyTiger2C y deben tener un mensaje que describe el error y es impreso por el script pytiger2c.

--- packages/pytiger2c/__init__.py
+++ packages/pytiger2c/__init__.py
@@ -8,7 +8,9 @@ Tiger que genera código en lenguaje C. Opcionalmente, el código C resultante
 puede ser compilado con un compilador de C para generar un ejecutable.
 """
 
-from pytiger2c.errors import PyTiger2CError
+import codecs
+
+from pytiger2c.errors import PyTiger2CError, SyntacticError, SemanticError, CodeGenerationError 
 
 
 __version__ = '0.1'
@@ -19,9 +21,57 @@ __authors__ = (
 )
 
 
+def syntactic_analysis(input_fd):
+    """
+    Realiza análisis léxico-gráfico y sintáctico. 
+    
+    Si se encuentra algún error de sintáxis durante el análisis del programa se lanzará
+    una exepción C{SyntacticError} que contendrá información acerca del error.
+    
+    @type input_fd: C{file}
+    @param input_fd: Descriptor de fichero del programa Tiger a traducir.
+    @rtype: C{LanguageNode}
+    @return: Árbol de sintáxis abstracta correspondiente al programa Tiger.
+    """
+    raise SyntacticError()
+
+
+def check_semantics(syntax_tree):
+    """
+    Realiza comprobación semántica.
+    
+    Si se encuentra algún error semántico se lanzará una excepción C{SemanticError} que
+    contendrá información acerca del error. 
+    
+    @type syntax_tree: C{LanguageNode}
+    @param syntax_tree: Árbol de sintáxis asbtracta correspondiente a un programa Tiger.
+    """
+    raise SemanticError()
+
+
+def generate_code(syntax_tree, output_fd):
+    """
+    Realiza la generación de código.
+    
+    Si se produce algún error durante la generación de código se lanzará una excepción
+    C{CodeGenerationError} que contendrá información acerca del error.
+    
+    @type syntax_tree: C{LanguageNode}
+    @param syntax_tree: Árbol de sintáxis asbtracta correspondiente a un programa Tiger.
+    @type output_fd: C{file}
+    @param output_fd: Descriptor de fichero del archivo donde se generará el programa 
+        resultante de la traducción.
+    """
+    raise CodeGenerationError()
+    
+
 def translate(tiger_filename, c_filename):
     """
     Traduce un programa Tiger a un programa C equivalente.
+    
+    Se utiliza las funciones auxiliares C{syntactic_analysis}, C{check_semantics} 
+    y C{generate_code} para llevar a cabo cada una de las fases de la compilación 
+    del programa.
 
     @type tiger_filename: C{str}
     @param tiger_filename: Ruta absoluta al archivo que contiene el código
@@ -31,4 +81,14 @@ def translate(tiger_filename, c_filename):
         C resultante. Si existe un archivo en la ruta especificada este será
         sobreescrito.
     """
-    raise PyTiger2CError()
+    try:
+        with codecs.open(tiger_filename, encoding='utf-8', mode='rb') as input_fd: 
+            syntax_tree = syntactic_analysis(input_fd)
+    except IOError:
+        raise PyTiger2CError(error_msg='Could not open the Tiger input file')
+    check_semantics(syntax_tree)
+    try:
+        with codecs.open(c_filename, encoding='utf-8', mode='wb') as output_fd:
+            generate_code(syntax_tree, output_fd)
+    except IOError:
+        raise PyTiger2CError(error_msg='Could not open the output file')
--- packages/pytiger2c/errors.py
+++ packages/pytiger2c/errors.py
@@ -1,11 +1,46 @@
 # -*- coding: utf-8 -*-
 
 """
-Contiene la jerarquía de las excepciones lanzadas en el paquete.
+Jerarquía de las excepciones lanzadas en el paquete.
 """
 
 
 class PyTiger2CError(Exception):
     """
-    Clase base de todas las excepciones lanzadas en el paquete.
+    Base de todas las excepciones lanzadas en el paquete.
     """
+    
+    def __init__(self, error_type='Error', error_msg='An error occurred during the execution of PyTiger2C'):   
+        super(PyTiger2CError, self).__init__()
+        self._error_type = error_type
+        self._error_msg = error_msg
+        
+    def __str__(self):
+        return '{type}: {msg}.'.format(type=self._error_type, msg=self._error_msg)        
+        
+
+class SyntacticError(PyTiger2CError):
+    """
+    Excepción lanzada durante el análisis léxico-gráfico y sintáctico.
+    """
+    
+    def __init__(self, error_msg='An error occurred during the syntax analysis'):
+        super(SyntacticError, self).__init__('Syntactic Error', error_msg)
+
+
+class SemanticError(PyTiger2CError):
+    """
+    Excepción lanzada durante el análisis semántico.
+    """
+    
+    def __init__(self, error_msg='An error occurred during the semantic analysis'):
+        super(SemanticError, self).__init__('Semantic Error', error_msg)
+
+
+class CodeGenerationError(PyTiger2CError):
+    """
+    Excepción lanzadas durante la generación de código.
+    """
+    
+    def __init__(self, error_msg='An error occurred during the code generation'):
+        super(CodeGenerationError, self).__init__('Code Generation Error', error_msg)
