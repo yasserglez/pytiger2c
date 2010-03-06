3c16a283602421b1d69ae65ac3a9087c49709108
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-03-05 19:57:20 -0500

    Implementando operadores de igualdad y desigualdad entre tipos de Tiger.

--- packages/pytiger2c/types/tigertype.py
+++ packages/pytiger2c/types/tigertype.py
@@ -17,3 +17,24 @@ class TigerType(object):
         Inicializa la clase base de la jerarquía.
         """
         super(TigerType, self).__init__()
+        
+    def __eq__(self, other):
+        """
+        Implementación por defecto de la comparación entre los tipos del lenguaje Tiger. 
+        Dos tipos serán iguales si ambos son instancia de la misma clase. Es posible
+        que clases descendientes de esta definan esta comparación de otra manera.
+        
+        @type other: C{TigerType}
+        @param other: Otro tipo de Tiger con el que efectuar la comparación.
+        """
+        return isinstance(other, self.__class__)
+        
+    def __ne__(self, other):
+        """
+        Esta comparación se define como la negación del resultado obtenido por C{__eq__}.
+        Ver documentación del método C{__eq__} para más información.
+        
+        @type other: C{TigerType}
+        @param other: Otro tipo de Tiger con el que efectuar la comparación.        
+        """
+        return not self.__eq__(other)    
